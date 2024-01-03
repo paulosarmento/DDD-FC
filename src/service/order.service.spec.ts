57273499c2a554e65f0780abf1a30e35e70ea8f0
PauloSarmento <paulo.cesar.sarmento@hotmail.com>
2024-01-03 09:53:56 -0300

    Realizando Calculo com OrderService

--- src/service/order.service.spec.ts
+++ src/service/order.service.spec.ts
@@ -1,8 +1,9 @@
 import Order from "../entity/order";
 import OrderItem from "../entity/order_item";
+import OrderService from "./order.service";
 
 describe("Order service unit tests", () => {
-  it("Sould get total of all orders", () => {
+  it("Should get total of all orders", () => {
     const item1 = new OrderItem("i1", "Item 1", 100, "p1", 1);
     const item2 = new OrderItem("i2", "Item 2", 200, "p2", 2);
 
