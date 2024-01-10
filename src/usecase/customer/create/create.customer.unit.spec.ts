0e58b7e089a2a041285d1e02d68634a824d199f5
PauloSarmento <paulo.cesar.sarmento@hotmail.com>
2024-01-10 13:22:55 -0300

    Testes de criar customer

--- src/usecase/customer/create/create.customer.unit.spec.ts
+++ src/usecase/customer/create/create.customer.unit.spec.ts
@@ -37,4 +37,23 @@ describe("Unit Test create customer use case", () => {
       },
     });
   });
+
+  it("Should thrown an error when name is missing", async () => {
+    const customerRepository = MockRepository();
+    const customerCreateUseCase = new CreateCustomerUseCase(customerRepository);
+
+    input.name = "";
+    await expect(customerCreateUseCase.execute(input)).rejects.toThrow(
+      "Name is required"
+    );
+  });
+  it("Should thrown an error when street is missing", async () => {
+    const customerRepository = MockRepository();
+    const customerCreateUseCase = new CreateCustomerUseCase(customerRepository);
+
+    input.address.street = "";
+    await expect(customerCreateUseCase.execute(input)).rejects.toThrow(
+      "Street is required"
+    );
+  });
 });
